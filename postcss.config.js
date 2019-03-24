cd75fd6e060f481498c18a6c3d8a6d619452b8d5
mio tagami <cre30r0ad@gmail.com>
2019-03-25 01:52:54 +0900

    vueのブランチとしての土台持ってくる

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,7 @@
-const readConfig = require('read-config');
-
-// base config
-const SRC = './src'
-const DEST = './public'
-
-const constants = readConfig(`${SRC}/constants.yml`)
-const { IMG_DIR } = constants
-
 module.exports = {
   plugins: [
     // ベンダープレフィックスを自動付与する
     require('autoprefixer')(),
-    require('css-mqpacker')(),
-    require('postcss-assets')({
-        basePath: `${DEST}`,
-        loadPaths: [`.${IMG_DIR}/`],
-    })
+    require('css-mqpacker')()
   ],
 };
---
+++ src/vue/pages/Home.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.home
+    h1 {{msg}}
+</template>
+
+<script>
+export default {
+    name: "home",
+    data() {
+        return {
+            msg: 'hello world!'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "common";
+</style>
